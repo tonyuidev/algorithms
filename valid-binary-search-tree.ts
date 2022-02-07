a40b5bacea6e3f5fb2e729342ae67b6e193ff741
tonyuidev <99224556+tonyuidev@users.noreply.github.com>
2022-02-07 18:49:22 -0500

    Valid Binary Tree
    Checks if tree is a valid binary tree using Typescript.

---
+++ valid-binary-search-tree.ts
@@ -0,0 +1,33 @@
+class NodeItem {
+    constructor( public value: number,
+        public left?: any,
+        public right?: any
+        ){}
+
+        nodeSolution(){
+            let isValid = this.isValidBST(this,-Infinity,Infinity)
+            return isValid
+        }
+
+        isValidBST(node: this, low: number,high: number){
+            if(!node){
+                return true
+            }
+            let val = node.value
+            if( (val > low && val < high) 
+            && (this.isValidBST(node.left,low,val)
+            && (this.isValidBST(node.right,val,high))
+            ) ){
+                return true
+            }
+            return false;
+            
+        }
+}
+
+let node = new NodeItem(5)
+node.left =  new NodeItem(4)
+node.right = new NodeItem(7)
+node.right.left = new NodeItem(2)
+
+console.log(node.nodeSolution())
