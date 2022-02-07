042dc312379e698c1aeb5ad441a6273234f1a234
tonyuidev <99224556+tonyuidev@users.noreply.github.com>
2022-02-07 18:54:25 -0500

    Update valid-binary-search-tree.ts

--- valid-binary-search-tree.ts
+++ valid-binary-search-tree.ts
@@ -1,34 +1,32 @@
 class NodeItem {
-    constructor( public value: number,
-        public left?: any,
-        public right?: any
-        ){}
+  constructor(public value: number, public left?: any, public right?: any) {}
 
-        nodeSolution(){
-            let isValid = this.isValidBST(this,-Number.MAX_VALUE,Number.MAX_VALUE)
-            return isValid
-        }
+  nodeSolution() {
+    let isValid = this.isValidBST(this, -Number.MAX_VALUE, Number.MAX_VALUE);
+    return isValid;
+  }
 
-        isValidBST(node: this, low: number,high: number){
-            if(!node){
-                return true
-            }
-            let val = node.value
-            if( (val > low && val < high) 
-            && (this.isValidBST(node.left,low,val)
-            && (this.isValidBST(node.right,val,high))
-            ) ){
-                return true
-            }
-            return false;
-            
-        }
+  isValidBST(node: this, low: number, high: number) {
+    if (!node) {
+      return true;
+    }
+    let val = node.value;
+    if (
+      val > low &&
+      val < high &&
+      this.isValidBST(node.left, low, val) &&
+      this.isValidBST(node.right, val, high)
+    ) {
+      return true;
+    }
+    return false;
+  }
 }
 
-let node = new NodeItem(5)
-node.left =  new NodeItem(4)
-node.right = new NodeItem(7)
-let valid = node.nodeSolution() // true
+let node = new NodeItem(5);
+node.left = new NodeItem(4);
+node.right = new NodeItem(7);
+let valid = node.nodeSolution(); // true
 
-node.right.left = new NodeItem(2)
-let valid = node.nodeSolution() // false
+node.right.left = new NodeItem(2);
+let valid = node.nodeSolution(); // false
